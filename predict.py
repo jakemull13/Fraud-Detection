86fd212e023240e68bdf1f291e852de8893c200a
Jacob Mullins <jacobmullins@Jacobs-MacBook-Air.local>
2020-05-28 17:32:46 -0400

    checkpoint

--- predict.py
+++ predict.py
@@ -1,9 +1,12 @@
 from joblib import dump, load
+import csv
+import numpy as np
+import pandas as pd
 
 
 with open('test_script_examples.csv') as fd:
     reader=csv.reader(fd)
-    interestingrow=[row for idx, row in enumerate(reader) if idx in (0,np.random.randint(1,100))][1]
+    interestingrow=[row for idx, row in enumerate(reader) if idx in (0,np.random.randint(1,99))][1]
 
 interestingrow
 
@@ -22,5 +25,8 @@ truth=pd.Series(l[-1])
 model = load('modelpickle.joblib')
 
 #predict
-prediction = model.predict_proba(np.array(input).reshape(1,-1))
+prediction = model.predict(np.array(input).reshape(1,-1))
+proba = model.predict_proba(np.array(input).reshape(1,-1))
 
+print(proba)
+#print(f'We predict that this is {prediction}. Chance of Fraud = {proba[1]} kjdsflkajsdlkfjlksjdflkajdsf')
